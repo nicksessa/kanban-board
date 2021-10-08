bc3dc58260537b7987d2ae76554721dd9362ddc7
NickSessa <nicholas.sessa@gmail.com>
2021-10-08 15:12:34 -0400

    Create drag and drop functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll(".solid");
 const addItemContainers = document.querySelectorAll(".add-container");
 const addItems = document.querySelectorAll(".add-item");
 // Item Lists
-const itemLists = document.querySelectorAll(".drag-item-list");
+const listColumns = document.querySelectorAll(".drag-item-list");
 const backlogList = document.getElementById("backlog-list");
 const progressList = document.getElementById("progress-list");
 const completeList = document.getElementById("complete-list");
@@ -21,6 +21,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -69,6 +71,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement("li");
   listEl.classList.add("drag-item");
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute("ondragstart", "drag(event)");
   // append
   columnEl.appendChild(listEl);
 }
@@ -100,6 +104,37 @@ function updateDOM() {
     createItemEl(onHoldList, 0, onHoldItem, index);
   });
   // Run getSavedColumns only once, Update Local Storage
+}
+
+// When item starts dragging
+function drag(e) {
+  draggedItem = e.target;
+  console.log("draggedItem: ", draggedItem);
+}
+
+// Column Allows for item to drop
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// when the item enters the column area
+function dragEnter(column) {
+  //console.log(listColumns[column]);
+  listColumns[column].classList.add("over");
+  currentColumn = [column];
+}
+
+// dropping item in column
+function drop(e) {
+  e.preventDefault();
+  // remove background color/padding
+  listColumns.forEach((column) => {
+    column.classList.remove("over");
+  });
+  // add item to column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 // on load
